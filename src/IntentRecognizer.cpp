601e4623775780572158bb02642762b98b359464
the <the@enda.eu>
2020-12-08 18:22:45 +0100

    Add IntentRecognizer::getIntent() method and add a naive implementation

--- src/IntentRecognizer.cpp
+++ src/IntentRecognizer.cpp
@@ -4,3 +4,18 @@ IntentRecognizer::IntentRecognizer()
 {
 
 }
+
+std::string IntentRecognizer::getIntent(const std::string &phrase) const
+{
+    if (phrase == "What is the weather like today?") {
+        return "Intent: Get Weather";
+    } else if (phrase == "What is the weather like in Paris today?" ||
+               phrase == "What is the weather like in New York today?") {
+        return "Intent: Get Weather City";
+    } else if (phrase == "Am I free at 13:00 PM tomorrow?") {
+        return "Intent: Check calendar";
+    } else if (phrase == "Tell me an interesting fact.") {
+        return "Intent: Get Fact";
+    }
+    return "Intent: unknown";
+}
--- src/IntentRecognizer.h
+++ src/IntentRecognizer.h
@@ -1,11 +1,14 @@
 #ifndef INTENTRECOGNIZER_H
 #define INTENTRECOGNIZER_H
 
+#include <string>
 
 class IntentRecognizer
 {
 public:
     IntentRecognizer();
+
+    std::string getIntent(const std::string &phrase) const;
 };
 
 #endif // INTENTRECOGNIZER_H
