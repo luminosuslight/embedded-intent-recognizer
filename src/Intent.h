9c8e02247c9b83c2d84736dea869603d2b397de9
the <the@enda.eu>
2020-12-09 17:45:01 +0100

    Add documentation to Intent class

--- src/Intent.h
+++ src/Intent.h
@@ -16,6 +16,21 @@ struct PatternRegex {
 class Intent
 {
 public:
+    /**
+     * @brief Intent repsents the intent an utterance can have
+     * @param name a short descriptive name
+     * @param patterns a list of patterns that are used to find phrases that match this intent
+     *
+     * The patterns are interpreted as regular expressions. In addition, there are three convenience keywords:
+     * - <word> is a placeholder for a single abtritary word
+     * - <text> is a placeholder for abritary text and includes the empty word
+     * - <attribute_name:[word|text]> can be used to store attributes from an input phrase and retreive
+     * them later.
+     *
+     * The patterns are OR connected: if one matches, this intent will have a high score.
+     * The score depends on the length of the matched pattern. If one intent matches a short pattern and another
+     * matches using a much longer pattern on the same input phrase, the latter intent is chosen.
+     */
     Intent(const std::string &name, const std::vector<std::string> &patterns);
 
     const std::string &getName() const;
