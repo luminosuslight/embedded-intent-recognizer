7c9c24998d7b44905fd9e0010b7cc672eab336ac
the <the@enda.eu>
2020-12-09 15:21:25 +0100

    Implement Intent class

--- src/Intent.cpp
+++ src/Intent.cpp
@@ -1,5 +1,7 @@
 #include "Intent.h"
 
+#include <iostream>
+
 Intent::Intent(const std::string &name, const std::vector<std::string> &patterns)
     : m_name(name)
     , m_patterns(patterns)
@@ -7,12 +9,57 @@ Intent::Intent(const std::string &name, const std::vector<std::string> &patterns
     compilePatternsToRegexes();
 }
 
-double Intent::getScore(const std::string &phrase) const
+const std::string &Intent::getName() const
 {
-
+    return m_name;
 }
 
+/**
+ * @brief Intent::getScore returns a score that says how well this intent suits the input phrase
+ * @param phrase the input phrase
+ * @return a score greater than or equal 0
+ */
+double Intent::getScore(const std::string &phrase) const
+{
+    for (const auto &regexItem: m_patternRegexes) {
+        const auto match = std::sregex_iterator(phrase.begin(), phrase.end(), regexItem.second);
+        if (match != std::sregex_iterator()) {
+            return regexItem.first;
+        }
+    }
+    return 0.0;
+}
+
+/**
+ * @brief Intent::compilePatternsToRegexes compiles the domain specific language used for the patterns to
+ * actual regex instances.
+ */
 void Intent::compilePatternsToRegexes()
 {
+    for (std::string pattern: m_patterns) {
+        const int maxScore = pattern.length();
+        // for named capturing patterns like "weather in <city:text>"
+        std::regex group("<[\\w:]+>");
+        std::regex namedGroup("<(\\w+):(\\w+)>");
+        int groupIndex = 1;
+        for (auto i = std::sregex_iterator(pattern.begin(), pattern.end(), group);
+             i != std::sregex_iterator(); ++i) {
+            const auto match = *i;
+            const auto matchedString = match.str(0);
+            const auto namedGroupMatch = std::sregex_iterator(matchedString.begin(), matchedString.end(), namedGroup);
+            if (namedGroupMatch != std::sregex_iterator()) {
+                pattern = pattern.replace(match.position(), match.length(), "<" + namedGroupMatch->str(2) + ">");
+                m_namedGroups.insert({groupIndex, namedGroupMatch->str(1)});
+            }
+            ++groupIndex;
+        }
 
+        // for normal placeholders like "<text> weather <text>"
+        pattern = std::regex_replace(pattern, std::regex("<text>\\s*"), "([\\w\\+\\-\\*\\\\,\\.:'äöüßÄÖÜ]+\\s*)*");
+        pattern = std::regex_replace(pattern, std::regex("<word>"), "([\\w.,]*)");
+
+        m_patternRegexes.push_back({maxScore, std::regex(pattern, std::regex_constants::icase)});
+    }
+    std::sort(m_patternRegexes.begin(), m_patternRegexes.end(),
+              [](const auto &lhs, const auto &rhs) { return lhs.first > rhs.first; });
 }
--- src/Intent.h
+++ src/Intent.h
@@ -1,14 +1,18 @@
 #ifndef INTENT_H
 #define INTENT_H
 
-#include <vector>
-#include <string>
+#include <map>
 #include <regex>
+#include <string>
+#include <vector>
+
 
 class Intent
 {
 public:
     Intent(const std::string &name, const std::vector<std::string> &patterns);
+
+    const std::string &getName() const;
 
     double getScore(const std::string &phrase) const;
 
@@ -18,7 +22,8 @@ private:
 protected:
     std::string m_name;
     std::vector<std::string> m_patterns;
-    std::vector<std::regex> m_patternRegexes;
+    std::vector<std::pair<int, std::regex>> m_patternRegexes;
+    std::map<int, std::string> m_namedGroups;
 };
 
 #endif // INTENT_H
