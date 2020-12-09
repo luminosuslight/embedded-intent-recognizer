247b99e41bab6ceae8cc64c99c895a94a1c12065
the <the@enda.eu>
2020-12-09 09:27:19 +0100

    Implement main method to forward command line argument to IntentRecognizer

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
-int main()
+#include "IntentRecognizer.h"
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!" << std::endl;
+    if (argc != 2) {
+        std::cout << "Usage: embedded_intent_recognizer_app \"input phrase\"" << std::endl;
+        return 0;
+    }
+
+    IntentRecognizer recognizer;
+
+    const auto intent = recognizer.getIntent(argv[1]);
+    std::cout << intent << std::endl;
+
     return 0;
 }
