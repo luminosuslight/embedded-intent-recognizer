b6f73a54f1f6d717031777a211953651a02dc96c
the <the@enda.eu>
2020-12-09 15:38:22 +0100

    Add IntentRecognizer::setupIntents() method

--- src/IntentRecognizer.cpp
+++ src/IntentRecognizer.cpp
@@ -2,7 +2,7 @@
 
 IntentRecognizer::IntentRecognizer()
 {
-
+    setupIntents();
 }
 
 std::string IntentRecognizer::getIntent(const std::string &phrase) const
@@ -18,4 +18,31 @@ std::string IntentRecognizer::getIntent(const std::string &phrase) const
         return "Intent: Get Fact";
     }
     return "Intent: unknown";
+}
+
+void IntentRecognizer::setupIntents()
+{
+    Intent weather("Get Weather", {
+                       "weather",
+                       "weather <text> today"
+                   });
+    m_intents.push_back(weather);
+
+    Intent weatherCity("Get Weather City", {
+                           "weather <text> in <city:text>",
+                           "will it rain in <city:text>"
+                       });
+    m_intents.push_back(weatherCity);
+
+    Intent calendar("Check calendar", {
+                        "am I free <text>",
+                        "appointment"
+                    });
+    m_intents.push_back(calendar);
+
+    Intent fact("Get Fact", {
+                    "fact",
+                    "tell <text> fact"
+                });
+    m_intents.push_back(fact);
 }
--- src/IntentRecognizer.h
+++ src/IntentRecognizer.h
@@ -3,12 +3,23 @@
 
 #include <string>
 
+#include "Intent.h"
+
 class IntentRecognizer
 {
 public:
     IntentRecognizer();
 
     std::string getIntent(const std::string &phrase) const;
+
+private:
+    /**
+     * @brief IntentRecognizer::setupIntents creates the intents that this system should understand
+     */
+    void setupIntents();
+
+protected:
+    std::vector<Intent> m_intents;
 };
 
 #endif // INTENTRECOGNIZER_H
